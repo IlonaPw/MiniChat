9d429bf372f961db5f82918e1ebc26916532a145
Ilona Peltier <i.looona.p@gmail.com>
2020-11-21 21:17:26 +0100

    minichat

---
+++ minichat.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MiniChat</title>
+</head>
+<header>
+
+    <h1>MiniChat</h1>
+</header>
+<body>
+    <form action="minichat_post.php" method="post">
+        <p>
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" name="pseudo" id="pseudo" maxlength="255" value="<?php if(isset($_COOKIE['pseudo'])) { echo $_COOKIE['pseudo']; } else { echo 'Entrez votre pseudo'; } ?>" size="20" />
+            <br/>
+            <label for="message">Message :</label>
+            <textarea type="text" name="message" id="message"rows="1" cols ="20">
+            </textarea>
+            <br/>
+            <input type="submit" value="Envoyer">
+        </p>
+    </form>
+    <fieldset>
+        <legend> MiniChat</legend>
+        <div id="chat">
+            <?php
+                try
+                {
+                    // On se connecte à MySQL
+                        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                }
+                catch(Exception $e)
+                {
+                    // En cas d'erreur, on affiche un message et on arrête tout
+                        die('Erreur : '.$e->getMessage());
+                }
+                $reponse = $bdd->query('SELECT * FROM minichat ORDER BY ID DESC ');
+                while($donnees = $reponse->fetch())
+                {
+                    ?>
+                    <p style="border-bottom: 2px black dotted;">
+                    <?php
+                    echo'<strong>'.htmlspecialchars($donnees['pseudo']).'</strong> :'.htmlspecialchars($donnees['message']);
+                    ?>
+                    </p>
+                    <?php
+                }
+                $reponse->closeCursor();
+            ?>
+        </div>
+    </fieldset>
+    <p>Pour voir les nouveaux messages :<a href="minichat.php" >Rafraichir</a>.</p>
+</body>
+<footer>
+</footer>
+</html>
---
+++ minichat_post.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_POST['pseudo'])) // Si le formulaire a été envoyé...
+{
+    setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true);
+    header("Location: minichat.php");
+}
+?>
+<?php
+try
+{
+	// On se connecte à MySQL
+        $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+if (isset($_POST['pseudo']) && isset($_POST['message']))
+{
+    // On ajoute une entrée dans la table minichat
+    $req = $bdd->prepare('INSERT INTO minichat(pseudo, message) VALUES(?, ?)');
+    $req->execute(array($_POST['pseudo'],$_POST['message']));
+    // redirige vers minichat.php
+    header('Location: minichat.php');
+}
+else {
+    echo '<a href="minichat.php" >Veuillez entrer des données valides !</a>';
+}
+
+?>
